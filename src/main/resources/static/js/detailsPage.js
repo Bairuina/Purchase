58bd56c5942b8e8a2e50c499fe21c765bf39c5d4
chf <1208120615@qq.com>
2019-10-02 08:09:41 +0800

    添加物流模态框

---
+++ src/main/resources/static/js/detailsPage.js
@@ -0,0 +1,18 @@
+$(".moreThan").on('click',function(){
+    var content = $(this).find("span").text();
+    if(content=="更多"){
+        $(this).find("span").text("收起");
+        $(this).parent().next().css('height',"auto");
+    }else{
+        $(this).find("span").text("更多");
+        $(this).parent().next().css('height',"70px");
+        if(arrows.src.match('up')){
+            arrows.src="images/down.png"
+        }
+    }
+});
+
+//搜索框对焦时
+$(".inputProduct").focus(function(){
+    $(this).children().children().css("");
+});
