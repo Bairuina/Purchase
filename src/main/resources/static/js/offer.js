08a5c00cec3f4036e6a6acff540ba3e6b5e2a198
chf <1208120615@qq.com>
2019-10-08 22:29:06 +0800

    offer页面提交交互完成

--- src/main/resources/static/js/offer.js
+++ src/main/resources/static/js/offer.js
@@ -1,5 +1,31 @@
-$('.select').on('click',function(){
-   $(this).next().slideToggle() ;
-   var selectText = $(this).children().find(span).text();
-   console.log(selectText);
+// $('.select').on('click',function(){
+//    $(this).next().slideToggle() ;
+//    var selectText = $(this).children().find(span).text();
+//    // console.log(selectText);
+// });
+//提交ajax请求
+$('.confirm').click(function () {
+
+   var number = $(".commodityNumber").data("number");
+   console.log(number);
+   var price = $('#offerInputContent').val();
+   console.log(price);
+   var content = $("#offerTextareaContent").val();
+   console.log(content);
+   $.ajax({
+      url:'/productoffer/offer',
+      data:{
+         xhbh:number,
+         price:price,
+         text:content
+      },
+      type:'PUT',
+      success:function (data) {
+         if(data.code==0){
+            alert(data.msg);
+         }else{
+            alert(data.msg)
+         }
+      }
+   });
 });
