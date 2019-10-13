2b07244b384c1a5ed2ed4a2d812807f74272a94a
Bairuina <121522598@qq.com>
2019-10-13 16:59:34 +0800

    over

--- src/main/resources/static/js/modal.js
+++ src/main/resources/static/js/modal.js
@@ -90,39 +90,22 @@ function billEndSend(){
 	}
 }
 
-
-
+// 点击上传商品唯一标识码
+$(".AddIdentification").on('click',function () {
+	// // 获取订单编号和商品编号
+	var xhbh = $("this").parent().parent().children().eq(0).children().eq(0).text();
+	// var xhbh =123;
+	var ddbh = $(".ddbh").text();
+	// console.log($(".ddbh").text());
+	// 上传框显示并将ddbh和spbh值放到框内
+	$(".code_modal").slideUp(10);
+	$('#orderID').val(ddbh);
+	$('#QRcode_goodsID').val(xhbh);
+})
 //推送标识码
-function codeSend(){
-	var QR_orderID = $("#QRCode_orderID").val();
-	var QRcode_ModalID = $("#QRcode_ModalID").val();
-	var QRcode_onlyCodeID = $("#QRcode_onlyCodeID").val();
-	if((QRcode_onlyCodeID != '') && (url != '')){
-		$.ajax({
-			type:"post",
-			url:"",
-			dataType:"json",
-			data:{
-				"ddbh":QR_orderID,
-				"xhbh":QRcode_ModalID,
-				"wybs":QRcode_onlyCodeID,
-				"pic":url
-			},
-			success:function(data){
-				if(data.code == 1){
-					inform("您好，标识码已推送到商城！");
-				}else if(data.code != 1){
-					inform(data.msg);
-				}
-			},
-			error:function(){
-				inform("网络故障");
-			}
-		});
-	}else{
-		inform("您所填信息有误! 请检查后重新提交!");
-	}
-}
+// function codeSend() {
+//
+// }
 
 //发票开具模块
 function billStartreturn(){
@@ -141,13 +124,14 @@ function billSignreturn(){
 }
 
 function billSignSubimt(){
+
 	billEndSend();
 	billSignreturn();
 }
 
 //标识码模块
 function codeSendReturn(){
-	$(".code_modal").slideUp(10);
+	// $(".code_modal").slideUp(10);
 }
 
 function codeSendSubmit(){
@@ -198,6 +182,9 @@ $(function(){
 		$(".end_modal").slideDown(10);
 	})
 	$(".AddIdentification").click(function(){
+		var spxh = $("this").parent().parent().children().eq(0).children().eq(0).text();
+		var ddbh = $('.ddbh').text();
+		console.log(spxh);
 		billStartreturn();
 		billSignreturn();
 		$(".code_modal").slideDown(10);
@@ -222,7 +209,30 @@ $(function(){
 		billSignSubimt();
 	})
 	$(".codeSubmit_btn").click(function(){
-		codeSendSubmit();
+		var ddbh = $("#orderID").val();
+		var xhbh = $("#QRcode_onlyCodeID").val();
+		var wybs = $("#QRcode_goodsID").val();
+		var files = $("#image").prop('files');
+		console.log(ddbh,xhbh,wybs);
+		console.log(files[0]);
+		var formData = new FormData();
+		formData.append('myFileName', files[0]);
+		formData.append('ddbh', ddbh);
+		formData.append('xhbh', xhbh);
+		formData.append('wybs', wybs);
+		$.ajax({
+			url: '/productList/product',
+			type:'post',
+			data: formData,
+			contentType:false,
+			processData:false,
+			success:function (data) {
+				console.log(data.msg);
+			},
+			error:function(){
+				alert("信息上传失败！");
+			}
+		})
 	})
 
 })
