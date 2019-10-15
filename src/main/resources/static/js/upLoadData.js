d936bed395c64f163e1e37acede9178b3b4bd72f
王言 <614200978@qq.com>
2019-10-15 22:19:31 +0800

    订单页面交互11

--- src/main/resources/static/js/upLoadData.js
+++ src/main/resources/static/js/upLoadData.js
@@ -1,24 +1,24 @@
-var data=window.location.href;
-var orderDataDdbh=$("#orderDataDdbh").text();
-
-if(data.match("/order-data/selectOneOrder")){
-    console.log("开始更新订单。。。");
-    $.ajax({
-        url:"/order-data/upOrderData",
-        type:"POST",
-        dataType:"JSON",
-        data:{
-            ddbh:orderDataDdbh,
-        },
-        success:function(data){
-            console.log(data);
-            if(data.code=='0'){
-                alert("确认订单成功！")
-                window.onload;
-            }
-            else{
-                alert("确认订单失败！")
-            }
-        },
-    });
-}
+// var data=window.location.href;
+// var orderDataDdbh=$("#orderDataDdbh").text();
+//
+// if(data.match("/order-data/selectOneOrder")){
+//     console.log("开始更新订单。。。");
+//     $.ajax({
+//         url:"/order-data/upOrderData",
+//         type:"POST",
+//         dataType:"JSON",
+//         data:{
+//             ddbh:orderDataDdbh,
+//         },
+//         success:function(data){
+//             console.log(data);
+//             if(data.code=='0'){
+//                 console.log("确认订单成功！")
+//                 window.onload;
+//             }
+//             else{
+//                 console.log("确认订单失败！")
+//             }
+//         },
+//     });
+// }
