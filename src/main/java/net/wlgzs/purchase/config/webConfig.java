15f2d21ef90ad5806f318eb15ee303c24f833db1
王言 <614200978@qq.com>
2019-10-23 20:33:42 +0800

    图片虚拟路径配置

--- src/main/java/net/wlgzs/purchase/config/webConfig.java
+++ src/main/java/net/wlgzs/purchase/config/webConfig.java
@@ -1,14 +1,29 @@
 package net.wlgzs.purchase.config;
 
+import org.junit.Test;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
 @Component
-public class webConfig implements WebMvcConfigurer {
+public class webConfig implements WebMvcConfigurer  {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        //配置图片显示路径
+        String photoPath=System.getProperty("user.dir");
+//        photoPath=photoPath.replace('\\','/');
+        registry.addResourceHandler("/photoData/**") .addResourceLocations("file:"+photoPath+"/pictures/");
+
     }
+
+    @Test
+    public void kl(){
+        String photoPath=System.getProperty("user.dir");
+        photoPath=photoPath.replace('\\','/');
+        System.out.println(photoPath);
+    }
+
 }
+
+
