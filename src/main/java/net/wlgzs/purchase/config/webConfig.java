6aa651ab0d8e8e1a6f30808d47e03dcb2abc51d0
wyx <1479614887@qq.com>
2019-11-25 21:41:36 +0800

    wyx

--- src/main/java/net/wlgzs/purchase/config/webConfig.java
+++ src/main/java/net/wlgzs/purchase/config/webConfig.java
@@ -18,6 +18,10 @@ public class webConfig implements WebMvcConfigurer  {
         //配置验收单PDF路径
         String PDFPath=System.getProperty("user.dir");
         registry.addResourceHandler("/PDFData/**") .addResourceLocations("file:"+PDFPath+"/PDFData/");
+
+        //配置验收单word路径
+        String WordDataPath=System.getProperty("user.dir");
+        registry.addResourceHandler("/WordData/**") .addResourceLocations("file:"+WordDataPath+"/WordData/");
     }
 
     @Test
